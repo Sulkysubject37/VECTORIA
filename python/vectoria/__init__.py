d6231db474fec5b32e2a6e508c70dc23cb31d76f
Sulkysubject37 <arshad10867c@gmail.com>
2026-01-20 23:05:41 +0530

    fix(python): expose Runtime in package __init__

--- python/vectoria/__init__.py
+++ python/vectoria/__init__.py
@@ -1,4 +1,5 @@
 from .graph import Graph, Node, DType
+from .runtime import Runtime
 from .capabilities import get_system_capabilities, SystemCapabilities, Architecture
 
 __version__ = "1.1.2-delta"
