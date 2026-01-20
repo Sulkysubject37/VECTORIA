41dc68d3caafc13b4fbaa26b9b2b421f044bbcca
Sulkysubject37 <arshad10867c@gmail.com>
2026-01-21 02:38:50 +0530

    chore: bump package version to v1.2.1-sigma

--- python/vectoria/__init__.py
+++ python/vectoria/__init__.py
@@ -2,4 +2,4 @@ from .graph import Graph, Node, DType
 from .runtime import Runtime
 from .capabilities import get_system_capabilities, SystemCapabilities, Architecture
 
-__version__ = "1.1.2-delta"
+__version__ = "1.2.1-sigma"
